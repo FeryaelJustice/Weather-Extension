bae5b3a945c9c2a54444066dd52f67332417bbb4
FeryaelJustice <nano9gs@hotmail.es>
2023-09-18 13:39:01 +0200

    Added bootstrap and fixed fetch cors and now works

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Weather-Extension
-A basic Google Chrome extension in HTML and JS Vanilla that prints the weather of one location.
+
+A basic Google Chrome extension in HTML and JS Vanilla that prints the weather of one location. By FeryaelDev.
+
+You MUST create an "environment.json" file in the root of the project and paste the content of "sampleEnvironment.json" with your own api key of openweathermap.org
--- script.js
+++ script.js
@@ -8,13 +8,7 @@ initProgram();
 function initProgram() {
     readTextFile("environment.json", async function (text) {
         let json = JSON.parse(text);
-        console.log(json)
-        const record = await fetchData(json);
-        try {
-            printData(record);
-        } catch (err) {
-            console.log(err);
-        }
+        fetchData(json);
     });
 }
 
@@ -33,15 +27,67 @@ async function readTextFile(file, callback) {
     rawFile.send(null);
 }
 
-async function fetchData(json) {
-    let API_KEY = json.api_key;
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=${API_KEY}`);
-    return await res.json();
+function fetchData(json) {
+    const API_KEY = json.api_key;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log("Your latitude: " + position.coords.latitude, " / Your longitude: " + position.coords.longitude);
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${API_KEY}`, {
+                method: 'GET',
+                headers: new Headers({
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                }),
+            }).then(res => {
+                if (res.status === 200) {
+                    console.log("Success on fetching data")
+                    return res.json();
+                }
+                else {
+                    console.error("Failed on fetching data")
+                    return null;
+                }
+            }).then(record => {
+                printData(record);
+            }
+            ).catch(err => {
+                console.log(`Error fetching data: ${err}`);
+            });
+        });
+    } else {
+        console.warn("Geolocation is not supported by this browser.");
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=${API_KEY}`, {
+            method: 'GET',
+            headers: new Headers({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            }),
+        }).then(res => {
+            if (res.status === 200) {
+                console.log("Success on fetching data")
+                return res.json();
+            }
+            else {
+                console.error("Failed on fetching data")
+                return null;
+            }
+        }).then(record => {
+            printData(record);
+        }
+        ).catch(err => {
+            console.log(`Error fetching data: ${err}`);
+        });
+    }
+
 }
 
 function printData(record) {
     document.getElementById("name").innerHTML = record.name;
     document.getElementById("main").innerHTML = record.weather[0].main;
     document.getElementById("description").innerHTML = record.weather[0].description.toUpperCase();
-    document.getElementById("temp").innerHTML = record.main.temp;
+    document.getElementById("temp").innerHTML = record.main.temp + "°C";
 }
